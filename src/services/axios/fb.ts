e79dc57a3881635f494e04339015447d86b8da43
aaam-designs <aaamdesigns@gmail.com>
2024-02-10 12:25:28 +0530

    chnages

--- src/services/axios/fb.ts
+++ src/services/axios/fb.ts
@@ -57,6 +57,7 @@ const fbService = {
       const response = await axios.get(
         `${baseUrl}/${userId}/accounts?access_token=${userAccessToken}`
       );
+      console.log("ggettpages", response.data);
       return response.data.data;
     } catch (err) {
       console.log("err", err);
